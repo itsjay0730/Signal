af05d0bd347a0c2b41ba06435f5a3c88415884ce
Jay Patel <itsjay0730@gmail.com>
2026-04-03 18:04:52 -0500

    removed unnecessary comments

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
 from api.routes import router
-# app = FastAPI()
-# @app.get("/")
-# def root():
-#     return {"message": "Signal up and running :)"}
 
 app = FastAPI()
-
 app.include_router(router)
