c159c41e90b80251127bc1b488d6d38746d6c215
Jay Patel <itsjay0730@gmail.com>
2026-04-04 16:49:21 -0500

    gnews api one!

--- backend/pipelines/news/fetch_news.py
+++ backend/pipelines/news/fetch_news.py
@@ -5,15 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
-#for hacker news fetch
-BASE_URL = "https://hacker-news.firebaseio.com/v0"
+GNEWS_API_KEY = os.getenv("GNEWS_API_KEY")
 
 def fetch_news():
     news = []
 
-    #################
+    ##################################
     #  NEWS API 
-    #################
+    ##################################
 
     #fetch newsapi tech related data
     techUrl = f"https://newsapi.org/v2/top-headlines?category=technology&pageSize=30&apiKey={NEWS_API_KEY}"
@@ -27,7 +26,7 @@ def fetch_news():
                 "title": article["title"].split(" - ")[0],
                 "source": article["source"]["name"],
                 "url": article["url"],
-                "category": "tech"
+                "category": "technology"
             })
 
     #fetch newsapi business related data
@@ -45,21 +44,22 @@ def fetch_news():
                 "category": "business"
             })
 
-    #################
+    ##################################
     #  HACKERNEWS API 
-    #################
-
-    categories = [
+    ##################################
+    
+    hackerCategories = [
         "/topstories.json",
         "/newstories.json",
         "/beststories.json",
     ]
+    HACKER_URL = "https://hacker-news.firebaseio.com/v0"
 
     allIds = []
 
     #collect IDs from all categories
-    for category in categories:
-        response = requests.get(BASE_URL + category)
+    for category in hackerCategories:
+        response = requests.get(HACKER_URL + category)
 
         if response.status_code == 200:
             ids = response.json()[:15]
@@ -69,7 +69,7 @@ def fetch_news():
 
     #fetch each story
     for storyId in allIds:
-        storyResponse = requests.get(BASE_URL + f"/item/{storyId}.json")
+        storyResponse = requests.get(HACKER_URL + f"/item/{storyId}.json")
 
         if storyResponse.status_code == 200:
             story = storyResponse.json()
@@ -82,10 +82,28 @@ def fetch_news():
                     "category": "tech"                   
                 })
 
-    #################
+    ##################################
     #  GNEWS API 
-    #################
+    ##################################
 
+    gnewsCategories = [
+        "technology",
+        "business",
+    ]
+
+    for category in gnewsCategories:
+        gnewsResponse = requests.get(f"https://gnews.io/api/v4/top-headlines?category={category}&lang=en&max=20&apikey={GNEWS_API_KEY}")
+
+        if gnewsResponse.status_code == 200:
+            articles = gnewsResponse.json()["articles"]
+
+            for article in articles:
+                news.append({
+                    "title": article["title"].split(" - ")[0],
+                    "source": article["source"]["name"],
+                    "url": article["url"],
+                    "category": category
+                })
 
 
     return news
