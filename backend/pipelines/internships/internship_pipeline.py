c6540441810f90d6b2fc2eef1d4c2a1a35059cb0
Jay Patel <itsjay0730@gmail.com>
2026-04-22 10:43:40 -0500

    Internship_pipeline done

---
+++ backend/pipelines/internships/internship_pipeline.py
@@ -0,0 +1,7 @@
+from .fetch_internships import fetch_internships
+
+#Kept this file just in case I need to add more
+#to this pipeline later, makes it easy
+
+def run_internship_pipeline():
+    return fetch_internships()
