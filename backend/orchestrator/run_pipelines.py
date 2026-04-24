f935ebd88c3030f50993a3106c247359271af0fd
Jay Patel <itsjay0730@gmail.com>
2026-04-23 23:26:04 -0500

    imported thread executor

--- backend/orchestrator/run_pipelines.py
+++ backend/orchestrator/run_pipelines.py
@@ -1,5 +1,6 @@
 from pipelines.internships.internship_pipeline import run_internship_pipeline
 from pipelines.news.news_pipeline import run_news_pipeline
+from concurrent.futures import ThreadPoolExecutor
 
 def run_all_pipelines():
 
