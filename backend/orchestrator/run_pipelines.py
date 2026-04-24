51d84575e2a497d52f2fa71386794a859a3e62d0
Jay Patel <itsjay0730@gmail.com>
2026-04-23 23:29:19 -0500

    commented

--- backend/orchestrator/run_pipelines.py
+++ backend/orchestrator/run_pipelines.py
@@ -2,8 +2,9 @@ from pipelines.news.news_pipeline import run_news_pipeline
 from pipelines.internships.internship_pipeline import run_internship_pipeline
 from concurrent.futures import ThreadPoolExecutor
 
+#Use thread executor to concurrently
+#call both pipelines
 def run_all_pipelines():
-
     with ThreadPoolExecutor as executor:
         news = executor.submit(run_news_pipeline)
         internships = executor.submit(run_internship_pipeline)
