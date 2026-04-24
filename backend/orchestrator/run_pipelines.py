fa42ebccddd24fa1b348e850d7391653c15af182
Jay Patel <itsjay0730@gmail.com>
2026-04-23 23:28:47 -0500

    concurrently call both pipelines

--- backend/orchestrator/run_pipelines.py
+++ backend/orchestrator/run_pipelines.py
@@ -1,7 +1,18 @@
-from pipelines.internships.internship_pipeline import run_internship_pipeline
 from pipelines.news.news_pipeline import run_news_pipeline
+from pipelines.internships.internship_pipeline import run_internship_pipeline
 from concurrent.futures import ThreadPoolExecutor
 
 def run_all_pipelines():
 
-    return
+    with ThreadPoolExecutor as executor:
+        news = executor.submit(run_news_pipeline)
+        internships = executor.submit(run_internship_pipeline)
+
+        try:
+            news = news.result()
+            internships = internships.result()
+        except Exception as e:
+            print(f"Pipeline error: {e}")
+            raise
+
+    return {"news": news, "internships": internships}
