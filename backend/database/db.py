3b6dcaeb253600f32c54f8e683403cc9aafeaf5d
Jay Patel <itsjay0730@gmail.com>
2026-05-11 21:07:09 -0500

    save signals

--- backend/database/db.py
+++ backend/database/db.py
@@ -8,3 +8,7 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_API_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def save_signals(signals: list):
+    response = supabase.table("signals").insert(signals).execute()
+    return response
