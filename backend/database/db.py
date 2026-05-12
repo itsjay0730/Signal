f62e31b4a55b02ac1415ddec6be3fc010c9cb250
Jay Patel <itsjay0730@gmail.com>
2026-05-11 21:13:17 -0500

    cmts p2

--- backend/database/db.py
+++ backend/database/db.py
@@ -9,10 +9,12 @@ SUPABASE_KEY = os.getenv("SUPABASE_API_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+#save the signals
 def save_signals(signals: list):
     response = supabase.table("signals").insert(signals).execute()
     return response
 
+#save the internships
 def save_internships(internships: list):
     response = supabase.table("internships").insert(internships).execute()
     return response
