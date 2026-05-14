15a422e484b68a1ebcf43ed96f0b04f1561a09d8
Jay Patel <itsjay0730@gmail.com>
2026-05-13 20:45:58 -0500

    cleaned up some stuff

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -8,7 +8,6 @@ router = APIRouter()
 def root():
     return {"message": "Signal backend running"}
 
-#future endpoint for the frontend
 @router.get("/signals")
 def get_signals():
     signals = get_all_signals()
